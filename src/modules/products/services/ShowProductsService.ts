5a446b88205ef4afb1a24a303cc5585ec92f81fd
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-02-13 20:01:16 -0500

    serviço para mostrar um produto

---
+++ src/modules/products/services/ShowProductsService.ts
@@ -0,0 +1,22 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import { Product } from "../typeorm/entities/Product";
+import { ProductsRepository } from "../typeorm/repositories/ProductsRepository";
+
+interface IRequest {
+    id: string;
+}
+
+export class ShowProductsService{
+    public async execute({ id }: IRequest): Promise<Product | undefined>{
+        const productsRepository = getCustomRepository(ProductsRepository);
+
+        const product =  productsRepository.findOne(id);
+
+        if(!product){
+            throw new AppError('Product not found');
+        }
+
+        return product;
+    }
+}
