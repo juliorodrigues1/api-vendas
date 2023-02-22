0e9cacba5c02a86f82d1946ce8283f69c3524006
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-02-21 19:32:58 -0500

    criando rotas de produtos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import productsRouter from "@modules/products/routes/products.routes";
+
 
 const routes = Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+
+routes.use("/products", productsRouter);
 
 export default routes;
