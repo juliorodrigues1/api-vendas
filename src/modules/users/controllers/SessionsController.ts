c3a8ed08d0a08e1cf5ccacd45c587530a3d2c432
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-03-13 20:48:35 -0500

    serviço de criar sessao

---
+++ src/modules/users/controllers/SessionsController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export class SessionsController{
+    public async create(request: Request, response: Response): Promise<Response>{
+        
+    }
+}
